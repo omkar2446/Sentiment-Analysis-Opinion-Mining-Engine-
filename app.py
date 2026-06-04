3d2c58ed84a1d5e0dbc4c931e98cea06aad3879c
omkar_2446 <otambe655@gmail.com>
2026-06-04 22:35:30 +0530

    change the host

--- app.py
+++ app.py
@@ -502,5 +502,6 @@ def predict_api():
         }), 500
 
 if __name__ == '__main__':
-    # Start the Flask development server
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    debug = os.environ.get('FLASK_ENV') == 'development'
+    app.run(host='0.0.0.0', port=port, debug=debug)
